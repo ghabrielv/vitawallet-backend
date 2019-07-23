cfa527dd29543217b69a8ef78bbe4d932b61189f
Ghabriel Villarreal <ghabrielv@gmail.com>
2019-07-23 16:21:28 -0400

    Improvement field fee

--- app/controllers/api/transactions_controller.rb
+++ app/controllers/api/transactions_controller.rb
@@ -3,7 +3,12 @@ module Api
         def create
             @fee = Fee.find(1)
             @transaction = Transaction.new(transaction_params)
-            @operation = Operation.new(amount: @transaction.amount, fee: Float(0), commission: Integer(0), total: Float(@transaction.amount / @fee.btc_buy), currency_receive: 'btc')
+
+            if @transaction.currency_send == 'usd' and @transaction.currency_receive == 'btc'
+              @operation = Operation.new(amount: @transaction.amount, fee: Float(@fee.btc_buy), commission: Integer(0), total: Float(@transaction.amount / @fee.btc_buy), currency_receive: 'btc')
+            else
+              @operation = Operation.new(amount: @transaction.amount, fee: Float(@fee.btc_sell), commission: Integer(0), total: Float(@transaction.amount / @fee.btc_sell), currency_receive: 'usd')
+            end
         
             if @operation.save and @transaction.save
               render json: {data: {id: Integer(@transaction.id), amount: Float(@transaction.amount), fee: @operation.fee, commission: @operation.commission, total: @operation.total, currency_receive: @operation.currency_receive, created_at: @transaction.created_at}, type: 'transactions'}
